9b280228dd7b724be4023df407d16e88c7175362
crice802 <rcrice@gmail.com>
2022-02-10 17:10:23 -0500

    fix date time

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,15 @@
 import datetime
 from django.db import models
+from django.utils import timezone
 
 def auto_now():
-    return datetime.datetime.now() - datetime.timedelta(days=1)
+    return timezone.now() - timezone.timedelta(days=1)
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(max_length=2048)
     posted_by = models.CharField(max_length=50, default="root")
-    posted_at = models.DateTimeField(default=auto_now)
+    posted_at = models.DateTimeField(auto_now=True)
     edited_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
