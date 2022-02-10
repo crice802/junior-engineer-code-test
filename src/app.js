4b4a6502ccce2777cf4ca098fc410bcd4ef6b3a3
crice802 <rcrice@gmail.com>
2022-02-10 11:27:09 -0500

    complete isValid

--- src/app.js
+++ src/app.js
@@ -15,11 +15,37 @@
  */
 function isValid (input, expectedCost, expectedOldestYear) {
   // Your code here
+  let cost 
+  
+  // compare each inputs and make sure they have all 
+  //fields set and all numbers are sensible i.e year xxxx 
+input.forEach(input => {
+  if (input.type !== 'Truck' | 'Trailer' | 'Tractor') {
+    return false
+  } else if (parseInt(input.year) > 2022 || parseInt(input.year) < parseInt(expectedOldestYear) ) {
+    return false
+  } else if (parseFloat(input.value) > parseFloat(expectedCost)) {
+    return false
+  }
+  return input
+})
+  //if the input is valid sum the value of each car and compare to expectedCost
+  if (input) {
+   for (let i = 0 ; i > input.length; i++) {
+     cost += input.value 
+     if (cost > expectedCost) {
+       return false
+     }
+   } 
+   return true 
+  }
 }
+
 
 /**
  * Prompt: Implement a few tests to validate that your function works as expected
  */
 function testIsValid () {
   // Your code here
+  
 }
